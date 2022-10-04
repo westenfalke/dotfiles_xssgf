14ff33083c5718ac20a82d9a88e54e9fd7526fd5
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-04 23:38:44 +0200

    added defaults in case CODESPACE_VSCODE_FOLDER is not set
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 !/bin/bash
 cat /workspaces/.codespaces/.persistedshare/dotfiles/add2.bashrc >> ~/.bashrc
 cp -fpn /workspaces/.codespaces/.persistedshare/dotfiles/.alias ~/
-cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/xssgf}
+cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/tmp}
 mkdir -vp ~/bin && cp -pp /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ~/bin
-mkdir -vp ${CODESPACE_VSCODE_FOLDER}/bin && cp -pn /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ${CODESPACE_VSCODE_FOLDER}/bin
+mkdir -vp ${CODESPACE_VSCODE_FOLDER:-/tmp}/bin && cp -pn /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ${CODESPACE_VSCODE_FOLDER:-/tmp}/bin
