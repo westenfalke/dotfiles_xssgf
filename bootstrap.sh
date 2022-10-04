dab53f7eb6441ba13a1f201ec6fda12a87d6423f
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-05 01:32:15 +0200

    script path for rebuild adjusted
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,7 @@ mkdir -vp ~/bin && cp -pp /workspaces/.codespaces/.persistedshare/dotfiles/*sh ~
 cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/$(echo $CODESPACE_NAME|cut -d'-' -f 3)}
 # xsh - eXtension of baSH
 /workspaces/.codespaces/.persistedshare/dotfiles/install-xsh-and-xssg.sh && /workspaces/.codespaces/.persistedshare/dotfiles/install-xssgf-xsh-lib-dev.sh
-# install pandoc apline from community repo if etc/apk/repositories exist
-# this ensures this script is only if the container is rebuild 
-# and prevents us from installing to install into non apline distro
+# install-pandoc.sh on apline only if etc/apk/repositories exist
+# this prevents the attempt to install in a non apline ditro .e.g. 
 # during setup during the first boot of the codespace
-if [ ! -e /etc/apk/repositories ]; then : /workspaces/.codespaces/.persistedshare/dotfiles/install-pandoc.sh; fi
+~/bin/install-pandoc.sh
