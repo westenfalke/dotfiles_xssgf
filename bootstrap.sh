39cf8df1b084b9fd1b194e3928fc383f053912a3
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-04 04:07:55 +0200

    spelling erros removed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,6 @@
 !/bin/bash
 cat /workspaces/.codespaces/.persistedshare/dotfiles/add2.bashrc >> ~/.bashrc
-cp -fpn /workspaces/.codespaces/.persistedshare/dotfiles/.alias >> ~/
+cp -fpn /workspaces/.codespaces/.persistedshare/dotfiles/.alias ~/
 cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/xssgf}
-mkdir -vfp ~/bin && cp -pp /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ~/bin
-mkdir -vfp ${CODESPACE_VSCODE_FOLDER}/bin && cp -pn /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ${CODESPACE_VSCODE_FOLDER}/bi
-
-n
+mkdir -vp ~/bin && cp -pp /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ~/bin
+mkdir -vp ${CODESPACE_VSCODE_FOLDER}/bin && cp -pn /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ${CODESPACE_VSCODE_FOLDER}/bin
