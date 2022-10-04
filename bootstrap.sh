01ec26fd9275421b019b2e4cbd7c2249b1dcedab
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-04 23:55:30 +0200

    cp Dockerfile, pandoc, install xsh and xssgf
    - Dockerfile and devcontainer.json for "apline" container
    - xsh - eXtension of baSH
    - a script to in install pandoc for apline from community repo
    - backups of the bootetrap.sh as well as install*.sh 
    
    
    Signed-off-by: Stefan Welke <7465621+westenfalke@users.noreply.github.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,10 @@
 cat /workspaces/.codespaces/.persistedshare/dotfiles/add2.bashrc >> ~/.bashrc
 cp -fpn /workspaces/.codespaces/.persistedshare/dotfiles/.alias ~/
 cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/tmp}
-mkdir -vp ~/bin && cp -pp /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ~/bin
-mkdir -vp ${CODESPACE_VSCODE_FOLDER:-/tmp}/bin && cp -pn /workspaces/.codespaces/.persistedshare/dotfiles/install*sh ${CODESPACE_VSCODE_FOLDER:-/tmp}/bin
+# a script to in install pandoc for apline from community repo
+# and backups of the bootetrap.sh as well as install*.sh 
+mkdir -vp ~/bin && cp -pp /workspaces/.codespaces/.persistedshare/dotfiles/*sh ~/bin
+# Dockerfile and devcontainer.json for "apline" container  
+cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/tmp}
+# xsh - eXtension of baSH
+/workspaces/.codespaces/.persistedshare/dotfiles/install-xsh-and-xssg.sh/ && /workspaces/.codespaces/.persistedshare/dotfiles/install-xssgf-xsh-lib-dev.sh
