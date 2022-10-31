a22c82cf6e2d804be197136d69ca9ec17583d4eb
Stefan Welke <7465621+westenfalke@users.noreply.github.com>
2022-10-31 20:37:49 +0000

    default $CODESPACE_VSCODE_FOLDER 2 $CODESPACE_NAME

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 cat /workspaces/.codespaces/.persistedshare/dotfiles/add2.bashrc >> ~/.bashrc
 cp -fpn /workspaces/.codespaces/.persistedshare/dotfiles/.alias ~/
-cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/$(echo $CODESPACE_NAME|cut -d'-' -f 3)}
+cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/$(cut -d'-' -f 3 <<< $CODESPACE_NAME)}
 # a script to in install pandoc for apline from community repo
 # and backups of the bootetrap.sh as well as install*.sh 
 mkdir -vp ~/bin && cp -p /workspaces/.codespaces/.persistedshare/dotfiles/*sh ~/bin
 # Dockerfile and devcontainer.json for "apline" container  
-cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/$(echo $CODESPACE_NAME|cut -d'-' -f 3)}
+cp -fprn /workspaces/.codespaces/.persistedshare/dotfiles/.devcontainer ${CODESPACE_VSCODE_FOLDER:-/workspaces/$(cut -d'-' -f 3 <<< $CODESPACE_NAME)}
 # xsh - eXtension of baSH
 /workspaces/.codespaces/.persistedshare/dotfiles/install-xsh-and-xssg.sh && /workspaces/.codespaces/.persistedshare/dotfiles/install-xssgf-xsh-lib-dev.sh
 # install-pandoc.sh on apline only if etc/apk/repositories exist
